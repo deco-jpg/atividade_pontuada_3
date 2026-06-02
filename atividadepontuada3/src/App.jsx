cafc8dabb8db603cae85f476f7d3bab4e77bc9a5
deco-jpg <srdaniloh@gmail.com>
2026-06-02 20:01:56 -0300

    Add files via upload

---
+++ atividadepontuada3/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './footer';
+import Header from './header';
+import Home from './pages/home';
+
+function App() {
+
+  return (
+    <>
+       <Header/>
+       <Home/>
+       <Footer/>
+    </>
+  )
+}
+
+export default App
